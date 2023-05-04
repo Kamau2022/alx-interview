cdf883247b0859fa1af2a3fef25cd2012479adb9
Benson Kamau <benkabenson@gmail.com>
2023-05-04 20:05:56 +0000

    to be fixed

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""technical interview
+"""
+import sys
+
+
+def minOperations(n):
+    """a function to find the minimum operation
+    """
+
+    sum = 0
+    i = 0
+    for i in range(1, n + 1):
+        sum = sum + 1
+        if sum == 3:
+            sum = 3 * 2
+            continue
+        if sum == 6:
+            sum = sum + 3
+            continue
+        if sum >= n:
+            break
+    return i

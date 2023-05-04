99a88ca67de5722d6822396f9d1ed48be204abad
Benson Kamau <benkabenson@gmail.com>
2023-05-04 21:38:03 +0000

    to be fixed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,16 +7,15 @@ def minOperations(n):
     """a function to find the minimum operation
     """
 
-    sum = 1
-    i = 0
+    sum = 0
     for i in range(1, n + 1):
-        sum = sum + 2
-        if sum == 9:
-            sum = 9 + 3
+        sum = sum + 1
+        if sum == 3:
+            sum = 3 * 2
             continue
         if sum == 6:
             sum = sum + 3
             continue
         if sum >= n:
             break
-    return i + 2
+    return i

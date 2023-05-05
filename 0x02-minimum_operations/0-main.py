f590dd4b63e0e69ee47a67cbb68a85e511f48ce1
Benson Kamau <benkabenson@gmail.com>
2023-05-05 14:27:47 +0000

    task done

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -2,12 +2,15 @@
 """
 Main file for testing
 """
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
 
 minOperations = __import__('0-minoperations').minOperations
 
-<<<<<<< HEAD
-n = 5.9
-=======
-n = 15
->>>>>>> 83df776a2e86b24565e4c3f32a948f16efd7ab93
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
 """technical interview
 """
+import math
 
 
 def minOperations(n):
-    """a function to find the minimum operation
+    """ a function to find minimum operations on a number
     """
-
-    if n is not int:
+    if n is None or n < 2:
         return 0
+    prime_factors = []
+    sum = 0
 
-    paste = 0
-    copy = 2
-    while (copy <= n):
-        if not (n % copy):
-            n = int(n / copy)
-            paste += copy
-            copy = 1
-        copy += 1
-    return paste
+    while n % 2 == 0:
+        prime_factors.append(2)
+        n = n / 2
+
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            prime_factors.append(i)
+            n = n / i
+
+    if n > 2:
+        prime_factors.append(n)
+
+    for i in prime_factors:
+        sum += i
+
+    return int(sum)

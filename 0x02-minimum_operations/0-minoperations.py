db77e31b179ae04c4f55471b02e50fca8362a86a
Benson Kamau <104911422+Kamau2022@users.noreply.github.com>
2023-05-05 07:43:15 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 """technical interview
 """
-import sys
 
 
 def minOperations(n):
     """a function to find the minimum operation
     """
 
-    sum = 0
-    i = 0
-    for i in range(1, n + 1):
-        sum = sum + 1
-        if sum == 3:
-            sum = 3 * 2
-            continue
-        if sum == 6:
-            sum = sum + 3
-            continue
-        if sum >= n:
-            break
-    return i
+    if n is not int:
+        return 0
+
+    paste = 0
+    copy = 2
+    while (copy <= n):
+        if not (n % copy):
+            n = int(n / copy)
+            paste += copy
+            copy = 1
+        copy += 1
+    return paste
